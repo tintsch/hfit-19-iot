6855e2f97b61a4bc1ea455d3817f6ef699425cfb
Valentin Keller <valentin.keller@silverlight.ch>
2021-05-11 12:32:21 +0200

    removed decoder from examples file, updated Excel-sheet

--- 2 Decoder RoomSensor/decoder_examples.js
+++ 2 Decoder RoomSensor/decoder_examples.js
@@ -1,36 +1,4 @@
-/*
-    Example payloads:
-    Payload: 04011809400302ea01050302be0000
-    Output:
-    {
-    "temperature": 23.28,
-    "co2": 490,
-    "humidity": 32
-    }
-*/
-
-// Decoder function for The Things Network:
-function Decoder(bytes, port) {
-  // bytes: Array
-  // port: LoRaWAN-Port
-  // Decode an uplink message from a buffer
-  // (array) of bytes to an object of fields.
-
-  // Teil Batterie (Alice)
-  var capacity = 2.6*(10*10*10*10*10*10);
-  var consumption = bytes[14]*(16*16*16*16) + bytes[13]*(16*16*16) + bytes[12]*(16*16) + bytes[11];
-  var battery = 100 / capacity * (capacity - consumption);
-
-  var decoded = {
-    humidity: bytes[4] * 0.5,
-    // temperature: (bytes[3] * 256 + bytes[2]) * 0.01    // Höherwertiges Byte multiplizieren mit 2 Stellen (16*16)
-    temperature: ((bytes[3] << 8) + bytes[2]) * 0.01,     // <<: shift left und zusammenzählen
-    // temperature: (bytes[3] << 8 | bytes[2]) * 0.01     // shift left und ODER Verknüpfung
-    co2: (bytes[8] << 8) + bytes[7],
-    battery: battery
-  };
-  return decoded;
-}
+// Beispiele für JSON
 
 // ODER: 1 oder 0 = 1
 // falsch oder falsch = falsch
